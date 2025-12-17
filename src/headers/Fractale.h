2bef1ff79bcf3cc3de86d3ffbfe4fcb0eee68220
ElitePat <nsundimbolipatrick@gmail.com>
2025-12-17 12:04:39 +0100

    Branche pour tester l'implementation en Orienté Objet

---
+++ src/headers/Fractale.h
@@ -0,0 +1,8 @@
+#ifndef FRACTALE_H
+#define FRACTALE_H
+
+
+#include <iostream>
+#include <vectors>
+
+#endif // FRACTALE_H
---
+++ src/headers/ImagePNG.h
@@ -0,0 +1,9 @@
+#ifndef IMAGEPNG_H
+#define IMAGEPNG_H
+
+
+#include <iostream>
+#include <vector>
+#include
+
+#endif // IMAGEPNG_H
